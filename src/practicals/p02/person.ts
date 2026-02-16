e93c705d6e839730529a0ed28169fe8272fedfba
patsaporn-phadungsil <68010852@kmitl.ac.th>
2026-02-16 10:19:46 +0700

    Update person.ts

--- src/practicals/p02/person.ts
+++ src/practicals/p02/person.ts
@@ -1,16 +1,16 @@
 export class Person {
-    public firstname :string = 'John';
-    public lastname : string = 'Doe';
+    public firstname :string = "John";
+    public lastname : string = "Doe";
     private age : number = 0 
 
-     static COUNTRY : string = 'Thailand';
+     static COUNTRY : string = "Thailand";
    
     setAge(age : number):void{
         this.age = age;
     }
   
    getFullName(){
-    return `${this.firstname}+" "+${this.lastname};`;
+    return this.firstname +" "+ this.lastname;
    } 
    getAge(){
    return this.age;
