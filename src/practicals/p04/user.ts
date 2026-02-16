2a54e98ecf35c858ca1c9aa97a4dbf3121a17ff6
patsaporn-phadungsil <68010852@kmitl.ac.th>
2026-02-16 10:12:41 +0700

    Update user.ts

--- src/practicals/p04/user.ts
+++ src/practicals/p04/user.ts
@@ -1,5 +1,17 @@
 export class User {
     public username : string
     private password :string 
-    private `${LOGIN_ATTEMPS}`
+    private LOGIN_ATTEMPTS : number
+
+
+    public User(username : string,password :string){
+
+    }
+    public login(password :string):boolean{
+
+    }
+    private (password) : boolean{
+
+    }
+    public getLoginAttempts() : number
 }
