c5f61a07c364a8deed061ab06d9fa7b3b19ae1e4
patsaporn-phadungsil <68010852@kmitl.ac.th>
2026-02-16 09:50:58 +0700

    kg

--- src/practicals/p02/person.ts
+++ src/practicals/p02/person.ts
@@ -1,1 +1,21 @@
-export class Person {}
+export class Person {
+    firstname :string = ''
+    lastname : string = ''
+    private age : number = 0 
+
+    public static COUNTRY : string = 'Thailand'
+    constructor(firstname : string,lastname :string){
+        this.firstname = firstname
+        this.lastname = lastname
+    }
+   
+    setAge(age : number):void{
+        this.age = age 
+    }
+   getAge():number{
+   return this.age
+   }
+   getFullName():string{
+    return `${this.firstname}${this.lastname}`
+   }
+}
