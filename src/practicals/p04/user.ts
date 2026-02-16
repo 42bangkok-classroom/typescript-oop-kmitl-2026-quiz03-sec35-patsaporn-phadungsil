f75290436710330009783d76c302495f35670a17
patsaporn-phadungsil <68010852@kmitl.ac.th>
2026-02-16 10:16:18 +0700

    Update user.ts

--- src/practicals/p04/user.ts
+++ src/practicals/p04/user.ts
@@ -1,17 +1,28 @@
 export class User {
     public username : string
     private password :string 
-    private LOGIN_ATTEMPTS : number
+    private LOGIN_ATTEMPTS : number = 0
 
+    constructor(){
+        this.username = username
+        this.password = password 
+           }
 
     public User(username : string,password :string){
 
     }
-    public login(password :string):boolean{
+     public login(password: string): boolean {
+    
+    this.LOGIN_ATTEMPTS++
 
-    }
-    private (password) : boolean{
+    return this.validatePassword(password)
+  }
 
-    }
-    public getLoginAttempts() : number
+  private validatePassword(password: string): boolean {
+    return this.password === password
+  }
+
+  public getLoginAttempts(): number {
+    return this.LOGIN_ATTEMPTS
+  }
 }
