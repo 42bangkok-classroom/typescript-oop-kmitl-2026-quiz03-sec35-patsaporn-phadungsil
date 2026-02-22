55e72e197fdeb5142d66080ec6e5c6b22ab7e195
patsaporn-phadungsil <68010852@kmitl.ac.th>
2026-02-22 15:18:05 +0700

    Update product.ts

--- src/practicals/p03/product.ts
+++ src/practicals/p03/product.ts
@@ -2,9 +2,9 @@ export class Product {
 public readonly name : string =""
 protected price : number = 0
 static readonly DISCOUNT_PERCENT = 10
-constructor(name : string,price :number){
-    this.name = name
-    this.price = price
+constructor(name? : string,price? :number){
+    this.name = name ?? ""
+    this.price = price ?? 0
 }
 getPrice():number{
 return this.price
